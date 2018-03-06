01793e0e21ca20f84bca13cfd84200370742c2b2
Cody Piersall <cody@arrc.ou.edu>
2018-03-05 21:02:32 -0600

    Explicitly rely on python3 instead of python.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-python ./create_symlinks.py -f
+python3 ./create_symlinks.py -f
 
