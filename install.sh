ca929da7d52f65232cfe8c22db8b10c6d4c70501
Cody Piersall <cody@arrc.ou.edu>
2018-04-13 11:11:32 -0500

    Add astextplain diff tool stuff.

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+# for the tool pdftotext, useful in git diffs
+sudo apt-get install poppler-utils
+
 python3 ./create_symlinks.py -f
 
