06211f79d7879400e03f3b2e0a823fbd52a7f833
Cody Piersall <cody@arrc.ou.edu>
2018-04-19 09:29:48 -0500

    Create symlinks for stuff in .config directory.

--- create_symlinks.py
+++ create_symlinks.py
@@ -6,10 +6,12 @@ import sys
 
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-EXCLUDE = {'.', '..', '.git', __file__, '.gitignore', 'install.sh'}
+EXCLUDE = {'.', '..', '.git', __file__, '.gitignore', 'install.sh', '.config'}
 HOME = os.environ['HOME']
 
-EXCLUDE_ENDINGS = ('~', '.un')
+EXCLUDE_ENDINGS = ('~', '.un', '.swp')
+DIRS = THIS_DIR, os.path.join(THIS_DIR, '.config')
+
 
 def exclude(fname):
     fname = os.path.basename(fname)
@@ -20,20 +22,22 @@ def exclude(fname):
             return True
     return False
 
-def main():
-    for file in os.listdir(THIS_DIR):
-        source = os.path.join(THIS_DIR, file)
-        symlink = os.path.join(HOME, file)
-        force = '-f' in sys.argv
-        if not exclude(source):
-            if not os.path.exists(symlink) or force:
-                if os.path.exists(symlink):
-                    os.remove(symlink)
-                os.symlink(source, symlink)
-                print('ln -s {} {}'.format(source, symlink))
-            else:
-                print('file {} already exists'.format(symlink))
 
+def main():
+    for dir in DIRS:
+        for file in os.listdir(dir):
+            source = os.path.join(dir, file)
+            whatsleft = source[len(THIS_DIR) + 1:]
+            symlink = os.path.join(HOME, whatsleft)
+            force = '-f' in sys.argv
+            if not exclude(source):
+                if not os.path.exists(symlink) or force:
+                    if os.path.exists(symlink):
+                        os.remove(symlink)
+                    os.symlink(source, symlink)
+                    print('ln -s {} {}'.format(source, symlink))
+                else:
+                    print('file {} already exists'.format(symlink))
 
 
 if __name__ == '__main__':
