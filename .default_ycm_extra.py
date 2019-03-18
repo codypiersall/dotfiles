b86d7cd9e3dc46b4289e904fc1437424f8aa872b
Cody Piersall <cody.piersall@gmail.com>
2019-03-18 09:11:44 -0500

    Better defaults for ycm.

--- .default_ycm_extra.py
+++ .default_ycm_extra.py
@@ -1,27 +1,23 @@
-import os
 
-
-def FlagsForFile(filename, **kwargs):
-    """
-
-    """
-    ext = os.path.splitext(filename)[1]
-
-    if ext == '.c':
-        lang = 'c'
-    elif ext.lower() in {'.c', '.cpp', '.cc'}:
-        lang = 'c++'
-    else:
-        return {'flags': []}
-
-    flags = ['-x', lang, '-Wall', '-Wextra', '-Werror', '-std=c99']
-
-    return {'flags': flags}
+CFLAGS = [
+    '-Wall',
+    '-Wextra',
+    '-Werror',
+    '-std=c99',
+]
 
 
 def Settings(**kwargs):
-    client_data = kwargs['client_data']
-    return {
-        'interpreter_path': client_data['g:ycm_python_interpreter_path'],
-        'sys_path': client_data['g:ycm_python_sys_path']
-    }
+    """
+
+    """
+    if kwargs['language'] == 'cfamily':
+        return {
+            'flags': CFLAGS
+        }
+    else:
+        return {'flags': []}
+
+    return
+
+
