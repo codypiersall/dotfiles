c6f98086be3531a1f5c27028319bbce90dac3d65
Cody Piersall <cody.piersall@gmail.com>
2019-03-27 20:05:12 -0500

    Use tmux plugin manager.

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+# don't forget to run install with Ctrl+B-I in Tmux!
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # for the tool pdftotext, useful in git diffs
 sudo apt-get install poppler-utils
