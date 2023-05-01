55982aeb2eb313ef7859f9039837ebf0168e5e84
Cody Piersall <cody@ou.edu>
2023-04-30 20:50:59 -0500

    Add -y to apt commands, install rust.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-sudo apt install curl
+sudo apt install -y curl
 if [ ! -e "$HOME/.vim/autoload/plug.vim" ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -17,17 +17,18 @@ fi
 
 mkdir ~/dev
 git clone https://Gogh-Co/Gogh ~/dev/Gogh
-sudo apt install dconf-cli uuid-runtime
+sudo apt install -y dconf-cli uuid-runtime
 
 git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 # don't forget to run install with Ctrl+B-I in Tmux!
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # for the tool pdftotext, useful in git diffs
-sudo apt install poppler-utils
-sudo apt install zsh
-sudo apt install vim
-sudo apt install xclip
+sudo apt install -y poppler-utils
+sudo apt install -y zsh
+sudo apt install -y vim
+sudo apt install -y neovim
+sudo apt install -y xclip
 
 # dev stuff
 sudo apt install build-essential python3-dev python3-virtualenvwrapper
@@ -37,3 +38,6 @@ touch ~/.zshrc_local
 touch ~/.bashrc_local
 mkdir -p ~/.goto
 mkdir ~/.vimjunk
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
