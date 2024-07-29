10c93ef109b12821af8462b558598bbd1cd47630
Cody Piersall <cody.piersall@gmail.com>
2024-07-29 12:57:53 -0500

    Make pathadd not add empty entries.

--- home/.common.sh
+++ home/.common.sh
@@ -60,6 +60,9 @@ function cgrep () {
 function pathadd() {
     if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
         PATH="$1:${PATH:+"$PATH:"}"
+        # remove double :: and trailing : (which add cwd to path)
+        PATH=$(echo $PATH | sed "s/::/:/g" | sed "s/:$//g")
+
     fi
 }
 
@@ -145,4 +148,3 @@ alias vimdiff='nvim -d'
 export CMAKE_EXPORT_COMPILE_COMMANDS=on
 export MAKEFLAGS="-j $(nproc)"
 export PYTHONBREAKPOINT=ipdb.set_trace
-
