cba1480d1b9cfa097414a4dfe014c6478c4e8ff7
Cody Piersall <cody@ou.edu>
2026-03-20 15:02:50 -0500

    Add init.lua.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,3 +5,7 @@ require("config.dap_config")
 vim.cmd([[
     source ~/.config/nvim/coc.vim
 ]])
+require('treesitter-modules').setup({
+    highlight = { enable = true },
+    incremental_selection = { enable = true },
+})
