e21cd887e5c5ae2d726aa74d1805e23fd71caf26
Cody Piersall <cody@ou.edu>
2026-03-20 14:57:56 -0500

    Use the treesitter-modules.nvim file.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,10 +3,25 @@ return {
         'nvim-treesitter/nvim-treesitter',
         lazy = false,
         build = ':TSUpdate',
-	opts = {
-	    ensure_installed = {
-        	"bash"
-	},
-}
-    }
+        opts = {
+            ensure_installed = {
+                "bash",
+                "c",
+                "c++",
+                "javascript",
+                "lua",
+                "python",
+                "rust",
+            },
+            highlight = {enabled = true },
+            indent = {enabled = true },
+        },
+    },
+    {
+        'MeanderingProgrammer/treesitter-modules.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        ---@module 'treesitter-modules'
+        ---@type ts.mod.UserConfig
+        opts = {},
+    },
 }
